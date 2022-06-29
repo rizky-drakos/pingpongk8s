1ed297da03dec2e5308a37a3137c7b18e55acc47
rizky-drakos <nhphat1997@gmail.com>
2022-06-29 20:30:25 +0700

    Fixed messages

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ echo "Waiting for the PONG service.." && sleep 10 >/dev/null
 # Now we deploy the ping cronjob and wait
 # for another 60s.
 kubectl apply -f ${TEMPLATES}/ping-cronjob.yaml
-echo "Waiting for the PONG service.." && sleep 60 >/dev/null
+echo "Waiting for the PING cronjob.." && sleep 60 >/dev/null
 
 echo "Logs of the following pods can be viewed with \`kubectl logs --follow <pod>\`:"
 kubectl get pods -l app=pong-deploy --no-headers | awk '{print $1}'
