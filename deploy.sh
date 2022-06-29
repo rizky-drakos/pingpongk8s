f8af451a93d3ea0dc118f86c5a894381e49960cc
rizky-drakos <nhphat1997@gmail.com>
2022-06-29 20:37:14 +0700

    Polished outputs

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,18 @@
 # Where we store our k8s templates.
 TEMPLATES="infra"
 
-# First we deploy the pong service
-# and wait 10s untill the service is ready.
+# First we deploy the pong service.
 kubectl apply -f ${TEMPLATES}/pong-deploy.yaml
 kubectl apply -f ${TEMPLATES}/pong-svc.yaml
-echo "Waiting for the PONG service.." && sleep 10 >/dev/null
 
 # Now we deploy the ping cronjob and wait
 # for another 60s.
 kubectl apply -f ${TEMPLATES}/ping-cronjob.yaml
-echo "Waiting for the PING cronjob.." && sleep 60 >/dev/null
+echo "Waiting for the PING cron service.." && sleep 60 >/dev/null
 
-echo "Logs of the following pods can be viewed with \`kubectl logs --follow <pod>\`:"
-kubectl get pods -l app=pong-deploy --no-headers | awk '{print $1}'
+echo "Logs of the following pods can be viewed with:"
+pods=$(kubectl get pods -l app=pong-deploy --no-headers | awk '{print $1}')
+for pod in ${pods}
+do
+    echo "+ kubectl logs --follow ${pod}" 
+done
